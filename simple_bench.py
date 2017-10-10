71614fed08f8e183a366ec2bf40e9055ded3762a
Chris Pattison <chpattison@gmail.com>
2017-10-10 02:40:15 -0500

    Fixed argument list

--- simple_bench.py
+++ simple_bench.py
@@ -15,9 +15,9 @@ print('Solving...')
 count = int(sys.argv[4])
 if count < 0:
     count = -count
-    temp_set = np.reciprocal(np.linspace(1./float(sys.argv[2]), 1./float(sys.argv[1]), -count))
+    temp_set = np.reciprocal(np.linspace(1./float(sys.argv[3]), 1./float(sys.argv[2]), -count))
 else:
-    temp_set = np.linspace(float(sys.argv[2]), float(sys.argv[1]), count)
+    temp_set = np.linspace(float(sys.argv[3]), float(sys.argv[2]), count)
 
 tts = solve.get_opt_tts(instances, temp_set)
 print(tts)
