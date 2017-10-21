46ba52bf1e6defab676e32b1157ff7b5a4b9dda9
Chris Pattison <cpattison@tamu.edu>
2017-10-20 21:04:59 -0500

    Fixed double negation of count in simple_bench

--- simple_bench.py
+++ simple_bench.py
@@ -14,7 +14,6 @@ instances = instance.get_size(sys.argv[1])
 print('Solving...')
 count = int(sys.argv[4])
 if count < 0:
-    count = -count
     temp_set = np.reciprocal(np.linspace(1./float(sys.argv[3]), 1./float(sys.argv[2]), -count))
 else:
     temp_set = np.linspace(float(sys.argv[3]), float(sys.argv[2]), count)
