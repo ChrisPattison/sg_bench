c265762a0c56d2b7b321ce72bee8b4711f5aba25
Chris Pattison <cpattison@tamu.edu>
2017-10-24 19:52:13 -0500

    Added instance source

--- bench.py
+++ bench.py
@@ -8,7 +8,7 @@ if len(sys.argv) < 4:
     print('simple_bench.py <instances> <T High> <T Low> <T Count>')
     quit()
 
-print('Loading instances...')
+print('Loading instances from '+sys.argv[1])
 instances = instance.get_size(sys.argv[1])
 print('Solving...')
 tts = solve.bench_tempering(instances, (float(sys.argv[2]), float(sys.argv[3])), int(sys.argv[4]))
