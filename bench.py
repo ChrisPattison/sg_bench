0c25dba0d8d245c2dbf764cdf04f613423c132cb
Chris Pattison <cpattison@tamu.edu>
2017-11-16 15:17:15 -0600

    Unified simple_bench and bench

--- bench.py
+++ bench.py
@@ -4,14 +4,19 @@ import instance
 import solve
 import numpy as np
 
-if len(sys.argv) < 4:
-    print('simple_bench.py <instances> <T High> <T Low> <T Count>')
-    quit()
+def bench(optimize_fields = True):
+    if len(sys.argv) < 4:
+        print('bench.py <instances> <T High> <T Low> <T Count>')
+        quit()
 
-print('Loading instances from '+sys.argv[1])
-instances = instance.get_size(sys.argv[1])
-print('Solving...')
-tts = solve.bench_tempering(instances, (float(sys.argv[2]), float(sys.argv[3])), int(sys.argv[4]))
-print(tts[0])
-print(tts[1])
-print('Median TTS: '+str(np.median(tts[0])*tts[1])+' us')
+    print('Loading instances from '+sys.argv[1])
+    instances = instance.get_size(sys.argv[1])
+    print('Solving...')
+    tts = solve.bench_tempering(instances, (float(sys.argv[2]), float(sys.argv[3])), int(sys.argv[4]), optimize_fields = optimize_fields)
+    print(tts[0])
+    print(tts[1])
+    print('Median TTS: '+str(np.median(tts[0])*tts[1])+' us')
+
+
+if __name__ == "__main__":
+    bench()
--- simple_bench.py
+++ simple_bench.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python
-import sys
-import instance
-import solve
-import numpy as np
+import bench
 
-if len(sys.argv) < 4:
-    print('simple_bench.py <instances> <T High> <T Low> <T Count>')
-    quit()
-
-print('Loading instances from '+sys.argv[1])
-instances = instance.get_size(sys.argv[1])
-print('Solving...')
-tts = solve.bench_tempering(instances, (float(sys.argv[2]), float(sys.argv[3])), int(sys.argv[4]), optimize_fields = False)
-print(tts[0])
-print(tts[1])
-print('Median TTS: '+str(np.median(tts[0])*tts[1])+' us')
+bench.bench(optimize_fields = False)
