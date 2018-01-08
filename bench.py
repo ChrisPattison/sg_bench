8267266e9c6b0bfe13566c9b711bff6f966bfa05
Chris Pattison <chpattison@gmail.com>
2018-01-07 18:45:21 -0600

    Moved basic input processing to seperate file

--- bench.py
+++ bench.py
@@ -1,46 +1,15 @@
 #!/usr/bin/python3
-import sys
-import instance
 import solve
+import pinput
 import numpy as np
-import json
-import io
-import pathlib
 
 def bench(optimize_temp = True):
-    if np.any(['-h' in s for s in sys.argv]) or len(sys.argv) < 2:
-        help = \
-'''Copyright (c) 2017 C. Pattison
-
-Configuration is the path to a JSON file that includes the following keys:
-
-instances : string
-beta :
-    count : int
-    min : float
-    max : float
-field_strength : float
-profile : float array
-
-bench.py <configuration>
-'''
-        print(help)
-        quit()
-
-
-    config_path = pathlib.Path(sys.argv[1])
-    config = {}
-    with io.open(str(config_path.resolve()), 'r') as config_file:
-        config = json.load(config_file)
-
-    instance_path = config_path.parents[0] / config['instances']
-    print('Loading instances from '+str(instance_path.resolve()))
-    instances = instance.get_instance_set(str(instance_path.resolve()))
+    config, instances = pinput.get_input('Benchmark solver')
     print('Solving...')
     tts = solve.bench_tempering(instances,\
         beta = (config['beta']['min'], config['beta']['max']), \
         temp_count = config['beta']['count'], \
-        field_strength = config['field_strength'], \
+        field_strength = config.get('field_strength', 1.0), \
         profile = config['profile'], \
         restarts = config.get('restarts', 100), \
         optimize_temp = optimize_temp)
---
+++ pinput.py
@@ -0,0 +1,36 @@
+import sys
+import instance
+import json
+import io
+import pathlib
+import numpy as np
+
+def get_input(help_text):
+    if np.any(['-h' in s for s in sys.argv]) or len(sys.argv) < 2:
+        help = 'Copyright (c) 2017 C. Pattison\n' + help_text + \
+'''
+Configuration is the path to a JSON file that includes the following keys:
+
+instances : string
+beta :
+    count : int
+    min : float
+    max : float
+field_strength : float
+profile : float array
+
+<util>.py <configuration>
+'''
+        print(help)
+        quit()
+
+    config_path = pathlib.Path(sys.argv[1])
+    config = {}
+    with io.open(str(config_path.resolve()), 'r') as config_file:
+        config = json.load(config_file)
+
+    instance_path = config_path.parents[0] / config['instances']
+    print('Loading instances from '+str(instance_path.resolve()))
+    instances = instance.get_instance_set(str(instance_path.resolve()))
+    
+    return config, instances
