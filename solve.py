b405470cac42334e4e8eef94f19f1ee05a7f2eaf
Chris Pattison <chpattison@gmail.com>
2018-03-12 14:25:45 -0500

    Rework for modified replica exchange

--- solve.py
+++ solve.py
@@ -14,53 +14,67 @@ class solve:
 
         self._machine_readable = config['machine_readable']
 
-        self._field_max = config['field']['max']
-        self._field_min = config['field']['min']
-        self._field_count = config['field']['count']
-        self._field_set = config['field'].get('set', None)
-        self._field_distr = config['field'].get('distr', 'log')
+        self._replica_count = config['replica_count']
 
-        self._beta = config.get('beta', 10.0)
+        self._driver = self._get_param_set_values(config['driver'])
+        self._problem = self._get_param_set_values(config['problem'])
+        self._beta = self._get_param_set_values(config['beta'])
+
         self._mc_sweeps = config.get('mc_sweeps', 10)
 
         self._restarts = config.get('bench_restarts', 100)
         self._sweep_timeout = config.get('sweep_timeout', 65536)
-        self._optimize_fields = config.get('optimize_fields', False)
+        self._optimize_set = config.get('optimize_set', False)
         self._observable_sweeps = config.get('observable_sweeps', 4096)
         self._observable_timeout = config.get('observable_timeout', 3)
         self._thermalize_threshold = config.get('thermalize_threshold', 1e-3)
 
         self._gse_target = config.get('gse_target', 1.00)
         
-        if self._optimize_fields and self._field_set:
-            warnings.warn('Optimize fields true but field set provided')
+        if self._optimize_set and (
+            self._driver['set'] 
+            or self._problem['set'] 
+            or self._beta['set'] ):
+            warnings.warn('Optimize parameter set true but parameter set provided')
 
         self._detailed_log = {'beta':self._beta}
+
+    def _get_param_set_values(self, dictionary):
+        param_set = {}
+        param_set['max'] = dictionary['max']
+        param_set['min'] = dictionary['min']
+        param_set['set'] = dictionary.get('set', None)
+        param_set['distr'] = dictionary.get('distr', 'linear')
+        return param_set
 
     def _output(self, string):
         if not self._machine_readable:
             print(string)
 
-    def _get_initial_field_set(self):
-        if self._field_distr == 'linear':
-            return np.linspace(self._field_min, self._field_max, self._field_count)
+    def _get_initial_set(self, set_params):
+        if set_params['distr'] == 'linear':
+            return np.linspace(set_params['min'], set_params['max'], self._replica_count)
         else:
-            return np.exp(np.linspace(np.log(self._field_min), np.log(self._field_max), self._field_count))
+            return np.exp(np.linspace(np.log(set_params['min']), np.log(set_params['max']), self._replica_count))
 
-    def _make_schedule(self, sweeps, field_set = None):
+    def _make_schedule(self, sweeps, param_set = None):
+        if not param_set:
+            param_set = {}
+            param_set['driver'] = self._driver['set'] if self._driver['set'] else self._get_initial_set(self._driver)
+            param_set['problem'] = self._problem['set'] if self._problem['set'] else self._get_initial_set(self._problem)
+            param_set['beta'] = self._beta['set'] if self._beta['set'] else self._get_initial_set(self._beta)
         return pt_propanelib.make_schedule( \
                 sweeps = sweeps, \
-                field_set = field_set if field_set else self._get_initial_field_set(), \
-                beta = self._beta, \
+                param_set = param_set, \
                 mc_sweeps = self._mc_sweeps)
 
     # Get TTS given a field set and sweep count
-    def _get_tts(self, instances, field_set, cost = np.median):
+    def _get_tts(self, instances, param_set, cost = np.median):
         results = []
         for i in range(len(instances)):
             instances[i]['target_energy'] = instances[i]['ground_energy'] * self._gse_target
         
-        schedule = self._make_schedule(sweeps = self._sweep_timeout, field_set = field_set)
+        schedule = self._make_schedule(sweeps = self._sweep_timeout, param_set = param_set, mc_sweeps = self._mc_sweeps)
         instances = backend.get_backend().run_instances(schedule, instances, self._restarts, statistics=False)
 
         p_s = []
@@ -116,11 +130,11 @@ class solve:
         return True
 
     # Return observables with thermalization based on observable obs
-    def _get_observable(self, instances, obs, field_set):
+    def _get_observable(self, instances, obs, param_set):
         solver = backend.get_backend()
         for i in range(self._observable_timeout):
             sweeps = self._observable_sweeps
-            schedule = self._make_schedule(sweeps = sweeps, field_set = field_set)
+            schedule = self._make_schedule(sweeps = sweeps, param_set = param_set)
             instances = solver.run_instances(schedule, instances, restarts = 1)
             # check equillibriation
             if np.all(np.vectorize(lambda i, obs: self._check_thermalized(i['results'], obs))(instances, obs)):
@@ -132,15 +146,14 @@ class solve:
             self._output('Using '+str(sweeps)+' sweeps')
         return [i['results'][i['results']['Samples']==i['results']['Samples'].max()] for i in instances]
 
-    def _get_disorder_avg(self, instances, obs, field_set):
-        return pd.concat(self._get_observable(instances, '<E>', field_set)).groupby('Gamma').apply(np.mean).drop(columns=['Gamma']).reset_index()
-        # return pd.concat(self._get_observable(instances, '<E>', field_set)).groupby(['Gamma']).mean().reset_index()
+    def _get_disorder_avg(self, instances, obs, param_set):
+        return pd.concat(self._get_observable(instances, '<E>', param_set)).groupby('Gamma').apply(np.mean).drop(columns=['Gamma']).reset_index()
 
 
     # Given a particular step and a starting field, uniformly place fields
     def _get_field_set(self, distance, energy, min_field):
         fields = [min_field]
-        for i in range(self._field_count-1):
+        for i in range(self._replica_count-1):
             cost = lambda x: ((fields[-1] - x)*np.abs(energy(fields[-1]) - energy(x)) - distance)
             for i in range(5):
                 # Bias in starting value to get the positive incremen
@@ -159,6 +172,7 @@ class solve:
             cubic_energy(f) if bounds[0] < f and f < bounds[1] else linear_energy(f))
 
     # Selects a dT*dE step such that the final field is the one desired
+    # Broken
     def _get_optimized_fields(self, disorder_avg):
         self._output('Computing field set...')
         field_norm = disorder_avg['Gamma'].max()
@@ -167,21 +181,21 @@ class solve:
         disorder_avg['norm_<E>'] = disorder_avg['<E>'] / energy_norm
         # fit to disorder averaged E(field)
         energy = self._interpolate_energy(disorder_avg['norm_Gamma'], disorder_avg['norm_<E>'])
-        residual = lambda step: (self._get_field_set(step, energy, self._field_min/field_norm)[-1] - self._field_max/field_norm)
+        residual = lambda step: (self._get_field_set(step, energy, self._driver_min/field_norm)[-1] - self._driver_max/field_norm)
         init_step = -(disorder_avg['norm_<E>'].max() - disorder_avg['norm_<E>'].min())*(disorder_avg['norm_Gamma'].max() - disorder_avg['norm_Gamma'].min())
         step = sp.optimize.bisect(residual, init_step*1e-5, init_step)
-        field_set = list(np.array(self._get_field_set(step, energy, self._field_min/field_norm)) * field_norm)
+        field_set = list(np.array(self._get_field_set(step, energy, self._driver_min/field_norm)) * field_norm)
 
         self._detailed_log['optimized_fields'] = field_set
         return field_set
 
     def observe(self, instances):
         self._output('Initial run...')
-        field_set = None
-        disorder_avg = self._get_disorder_avg(instances, '<E>', field_set)
-        if self._optimize_fields:
-            field_set = self._get_optimized_fields(disorder_avg)
-            disorder_avg = self._get_disorder_avg(instances, '<E>', field_set)
+        param_set = None
+        disorder_avg = self._get_disorder_avg(instances, '<E>', param_set)
+        if self._optimize_set:
+            param_set = self._get_optimized_fields(disorder_avg)
+            disorder_avg = self._get_disorder_avg(instances, '<E>', param_set)
         return disorder_avg
         
 
@@ -191,22 +205,25 @@ class solve:
     # Get optimal TTS
     def bench_tempering(self, instances):
         self._output('Computing observables...')
-        field_set = None
-        disorder_avg = self._get_disorder_avg(instances, '<E>', field_set)
+        param_set = {}
+        disorder_avg = self._get_disorder_avg(instances, '<E>', param_set)
         time_per_sweep = np.median(disorder_avg['Total_Walltime']/disorder_avg['Total_Sweeps'])
-        if self._field_set:
-            field_set = self._field_set
-        elif self._optimize_fields:
-            field_set = self._get_optimized_fields(disorder_avg)
-            self._output(field_set)
+
+        param_set['driver'] = self._driver['set']
+        param_set['problem'] = self._problem['set']
+        param_set['beta'] = self._beta['set']
+
+        if self._optimize_set:
+            param_set = self._get_optimized_params(disorder_avg)
+            self._output(param_set)
 
         self._output('Benchmarking...')
-        tts, success_prob, p99_tts = self._get_tts(instances, field_set)
+        tts, success_prob, p99_tts = self._get_tts(instances, param_set)
         self._detailed_log['time_per_sweep'] = time_per_sweep
         self._detailed_log['tts'] = tts
         self._detailed_log['p99_tts'] = p99_tts
         self._detailed_log['p_s'] = success_prob
-        self._detailed_log['field_set'] = self._field_set
+        self._detailed_log['set'] = param_set
         return tts, time_per_sweep
 
     def get_full_data(self):
