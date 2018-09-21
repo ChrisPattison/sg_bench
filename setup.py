fefee1fcb804baef0fbfdaca26ac41e99ec33746
Chris Pattison <chpattison@gmail.com>
2018-09-21 02:46:36 -0500

    Moved packages to src directory

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 from setuptools import setup, find_packages
 setup(
     name='sg_bench', 
-    packages=['sg_bench', 'quit_bench', 'pt_bench', 'psqa_bench'], 
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, 
     version='2.2', 
     description='Benchmarking suite for Heuristic Solvers',
     author='Chris Pattison',
     author_email='chpattison@gmail.com',
     install_requires=['numpy', 'scipy', 'pandas', 'paramiko', 'psutil'],
-    py_modules=find_packages())
+    )
